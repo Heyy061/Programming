6ee62097c17e0daf4de094f924706fd886e05dd8
Heyy061 <kr14427716@gmail.com>
2026-07-03 18:34:00 +0530

    Update 0283-move-zeroes.cpp

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-  int spaceCreate=0;
+    void moveZeroes(vector<int>& nums) {  //it is the most optimal approch , if we create a space and stored all zero in another array then it would 
+  int spaceCreate=0;               // be good approch
   int i=0;
   while(i<nums.size()){
    if(nums[i]!=0){
  nums[spaceCreate]=nums[i];  
         //jo vacant space create kiya h usme non zero no store karna h
      spaceCreate++;
-  } 
+  }        //time complexity is o(n) space is o(1)
   i++; 
   }
   while(spaceCreate<nums.size()){
