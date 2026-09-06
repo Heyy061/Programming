356867d19d8e966849e233402a2c5dfc9ae81635
Heyy061 <kr14427716@gmail.com>
2026-09-06 13:27:59 +0530

    Time: 98 ms (13.27%), Space: 24 MB (24.32%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -13,3 +13,20 @@ public:
     return sum;
     }
 };
+// optimal solution by prefix sum
+// class NumArray {
+// public:
+//     vector<int> prefix;
+
+//     NumArray(vector<int>& nums) {
+//         prefix.push_back(0);
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             prefix.push_back(prefix.back() + nums[i]);
+//         }
+//     }
+
+//     int sumRange(int left, int right) {
+//         return prefix[right + 1] - prefix[left]; 
+//     }
+// };
