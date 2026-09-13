42854c5bae3638b0208b9dae548d3ff9185228e8
Heyy061 <kr14427716@gmail.com>
2026-09-13 16:12:10 +0530

    Time: 0 ms (100%), Space: 35.8 MB (28.36%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -8,16 +8,17 @@ public:
 
        for(int i=0;i<n;i++){
         rightSum+=nums[i];
-       }
+       }  
 
        for(int i=0;i<n;i++){
          if(leftSum==rightSum-nums[i]){
             return i;
         }
-        leftSum+=nums[i];
-        rightSum-=nums[i];
+        leftSum+=nums[i];//ye value increase hoti jayegi
+        rightSum-=nums[i];//ye value decrease 
        
        }
 return -1;
     }
 };
+//logic is:- rightSum h ye  sum of all indexes h and we r adding indexes at left and removing in rightSum if case is not equal
